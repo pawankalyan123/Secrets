1b4b4f33f84f21c8e24f0add43c06ca830b0ebc2
Venkateswara reddy Chaikam <84827565+pawankalyan123@users.noreply.github.com>
2023-05-15 19:07:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/PostSecrets"
+    callbackURL: "https://perfect-pink-puppy.cyclic.app/auth/google/PostSecrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
